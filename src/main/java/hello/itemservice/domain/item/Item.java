ab64fa7f5d46791bfdd9ce44adf42443da4daf32
Link <mexlove2@gmail.com>
2023-09-15 22:08:23 +0900

    Spring thymeleaf

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -15,7 +15,7 @@ public class Item {
     private Boolean open; //판매여부
     private List<String> regions; //등록 지역
     private ItemType itemType;
-    private String deliveryType;
+    private String deliveryCode;
 
     public Item() {
     }
